6027ffff9b77d1acf348fe6b4d67f808d09c5862
Octa Leads <octaleadsprivatelimited@gmail.com>
2025-10-17 10:24:56 +0530

    Remove unused css import from Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import { FaBars, FaTimes, FaChevronDown } from 'react-icons/fa';
 
